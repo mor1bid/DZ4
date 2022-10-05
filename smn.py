67ac4d4cfed22e1bf0d89ffa946f291c75ea0240
De <spark49942@gmail.com>
2022-10-06 01:02:41 +0300

    DZ4

--- smn.py
+++ smn.py
@@ -11,7 +11,7 @@ while i<=N:
         i+=1
 print('множители данного числа:', multi)
 
-# multi = [1, 4, 2, 3, 3, 4, 2, 5, 7, 8, 8, 7, 9]
+# multi = [1, 1, 2, 3, 3, 4, 1, 5, 7, 8, 8, 7, 9]
 si = len(multi)
 x = 0
 y = 0
@@ -30,4 +30,29 @@ while x<si:
     total=1
     x+=1
     y=0
-print('3. Неповторяющиеся элементы:', mega, "\n ")
+print('3. Неповторяющиеся элементы:', mega)
+
+# import math
+import random
+k = int(input('4. Введите значение степени: '))
+x = []
+si = round(random.uniform(0,101))
+for i in range(int(si)):
+    xnum = round(random.uniform(0,101))
+    x.append(xnum)
+ix = x[si-1]
+form = round(2*pow(ix, k)+4*ix+5)
+data = open('DZ4.txt', 'a')
+data.write('k=')
+data.write(str(k))
+data.write(' => ')
+data.write('2*x^')
+data.write(str(k))
+data.write('+4*x+5=0 \n')
+data.writelines('with x=')
+data.write(str(ix))
+data.write(' => ')
+data.write(str(form))
+data.close()
+
+
