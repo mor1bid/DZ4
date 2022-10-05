069859ebf89da4984122ccc3d462df76d4f3ec21
De <spark49942@gmail.com>
2022-10-05 20:20:30 +0300

    DZ3-pzdz

--- smn.py
+++ smn.py
@@ -1,7 +1,7 @@
+
 N = int(input("2. Введите число: "))
 multi = []
 i = 2
-# for i in range(2,N+1):
 while i<=N:
     if N%i==0:
         N/=i
@@ -9,7 +9,39 @@ while i<=N:
         i=2
     else:
         i+=1
-        # if N%i==0:
-        #     multi.append(i)
-        #     # i=0
 print('множители данного числа:', multi)
+
+# multik = [1, 1, 2, 3, 3, 4, 1, 5, 7, 8, 8, 7, 9]
+si = len(multi)
+x = 0
+y = 0
+total = 1
+temp = multi[0]
+mega = []
+while x<si:
+    # if x==temp:
+    # else:
+    #     temp = i
+    # if multi[i]==temp:
+    #     i2=0
+    #     total+=1
+    #     temp=multi[i]
+    #     while i2<len(multi):
+    #         if i2==temp:
+    #             multi.remove(multi[i2])
+    #         i2+=1
+    while y<si:
+        if multi[x]!=multi[y]:
+            temp=multi[x]
+        else:
+            total+=1
+        y+=1
+                # print(temp)
+                # multi.pop(2)
+    if total==1:
+        mega.append(temp)
+    total=1
+    x+=1
+    y=0
+    # temp = multi[i]
+print('3.', mega, "\n ")
