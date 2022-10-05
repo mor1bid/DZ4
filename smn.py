23d42a10293aa3882d8ec9754e874d942ac0bd0a
De <spark49942@gmail.com>
2022-10-05 17:08:00 +0300

    DZ2-f

--- smn.py
+++ smn.py
@@ -1,7 +1,15 @@
-N = int(input("Введите число: "))
+N = int(input("2. Введите число: "))
 multi = []
-while N%2==0:
-    N/=2
-    multi.append(N)
-# multi.append(N)
-print(multi)
+i = 2
+# for i in range(2,N+1):
+while i<=N:
+    if N%i==0:
+        N/=i
+        multi.append(i)
+        i=2
+    else:
+        i+=1
+        # if N%i==0:
+        #     multi.append(i)
+        #     # i=0
+print('множители данного числа:', multi)
