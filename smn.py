5715876d7274264a67d3398b6bfb601941623d7c
De <spark49942@gmail.com>
2022-10-08 21:14:18 +0300

    DZ1

--- smn.py
+++ smn.py
@@ -1,36 +1,42 @@
+d = int(input('1. Введите точность вычисления числа π'))
+pi = 3,14
+if 10**-1 <= d <= 10**-10:
+    print(d+pi)
+else:
+    print('Задано некорректное значение')
 
-# N = int(input("2. Введите число: "))
-# multi = []
-# i = 2
-# while i<=N:
-#     if N%i==0:
-#         N/=i
-#         multi.append(i)
-#         i=2
-#     else:
-#         i+=1
-# print('множители данного числа:', multi)
+N = int(input("2. Введите число: "))
+multi = []
+i = 2
+while i<=N:
+    if N%i==0:
+        N/=i
+        multi.append(i)
+        i=2
+    else:
+        i+=1
+print('множители данного числа:', multi)
 
-# # multi = [1, 1, 2, 3, 3, 4, 1, 5, 7, 8, 8, 7, 9]
-# si = len(multi)
-# x = 0
-# y = 0
-# total = 1
-# temp = multi[0]
-# mega = []
-# while x<si:
-#     while y<si:
-#         if multi[x]!=multi[y]:
-#             temp=multi[x]
-#         else:
-#             total+=1
-#         y+=1
-#     if total==2:
-#         mega.append(temp)
-#     total=1
-#     x+=1
-#     y=0
-# print('3. Неповторяющиеся элементы:', mega)
+# multi = [1, 1, 2, 3, 3, 4, 1, 5, 7, 8, 8, 7, 9]
+si = len(multi)
+x = 0
+y = 0
+total = 1
+temp = multi[0]
+mega = []
+while x<si:
+    while y<si:
+        if multi[x]!=multi[y]:
+            temp=multi[x]
+        else:
+            total+=1
+        y+=1
+    if total==2:
+        mega.append(temp)
+    total=1
+    x+=1
+    y=0
+print('3. Неповторяющиеся элементы:', mega)
 
 import random
 k = int(input('4. Введите значение степени: '))
@@ -56,7 +62,7 @@ data.write(' \r\n')
 data.close()
 data = open('DZ4.txt', 'r')
 for li in data:
-    print(li, '\r\n')
+    print(li)
 data.close()
 
 data1 = open('DZ51.txt', 'r')
